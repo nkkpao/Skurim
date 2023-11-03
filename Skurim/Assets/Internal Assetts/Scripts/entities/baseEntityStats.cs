3c76bb2d86e0dc813dcd99a7e468c34e99452c5b
Ogn1k <2moestro2@gmail.com>
2023-11-03 20:45:39 +0700

    first commit✨✨✨
    -base entity
    -basic battle scene

---
+++ Skurim/Assets/Internal Assetts/Scripts/entities/baseEntityStats.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baseEntity 
+{
+
+    public int MaxHp { get; protected set; }
+    public int CurHp { get; protected set; }
+
+    public int Speed { get; protected set; }
+
+}
