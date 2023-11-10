833e8f00f3f130b3aada0438af047af9b14db524
Ogn1k <2moestro2@gmail.com>
2023-11-10 12:59:51 +0700

    Update CharAnimController.cs
    using UnityEditorInternal; delete test

--- Skurim/Assets/Internal Assetts/Scripts/CharAnimController.cs
+++ Skurim/Assets/Internal Assetts/Scripts/CharAnimController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UIElements;
 
